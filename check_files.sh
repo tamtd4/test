4c01812e70d47e7d734f0feedce6aececc6a57dd
tamtd4 <45614382+tamtd4@users.noreply.github.com>
2020-03-12 16:38:08 +0700

    Update check_files.sh

--- check_files.sh
+++ check_files.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
-PATH_REMOTE=/home/gitlab-runner/
+BACKUP_PATH=/home/gitlab-runner/test
+REMOTE_PATH=/home/gitlab-runner
 USER=gitlab-runner
 HOST=172.20.23.161
+
+cd $BACKUP_PATH
 
 declare -a dirs
 i=1
@@ -13,14 +16,15 @@ echo "There are ${#dirs[@]} dirs in the current path"
 
 for((i=1;i<=${#dirs[@]};i++))
 do
-    ssh $HOST  "test -e $PATH_REMOTE/${dirs[i]}"
+    echo $REMOTE_PATH/${dirs[i]}
+    ssh $HOST  "test -e $REMOTE_PATH/${dirs[i]}.tar.gz"
     if [ $? -eq 0 ]; then
-        echo "File ${dirs[i]} exists"
+        echo "File ${dirs[i]}.tar.gz exists"
     else
-        echo "File ${dirs[i]} does not exist"
-        tar zcvf - ${dirs[i]} | ssh $USER@$HOST "cat > $PATH_REMOTE/${dirs[i]}.tar.gz"
+        echo "File ${dirs[i]}.tar.gz does not exist"
+        tar zcvf - ${dirs[i]} | ssh $USER@$HOST "cat > $REMOTE_PATH/${dirs[i]}.tar.gz"
+    fi
 
-
-#    echo $i "${dirs[i]}"
+        echo $i "${dirs[i]}"
 #    echo  "${dirs[i]}"
 done
